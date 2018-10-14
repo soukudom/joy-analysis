ad954bbf67a87ed1ea522b1e41ce68e2d99b2892
Dominik Soukup <soukupdo@gmail.com>
2018-10-14 10:29:43 +0200

    New set of pcaps

--- create-data-sets.sh
+++ create-data-sets.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
 
-joy_dir="joy-data"
-data_set_dir="data-sets"
+src_joy_dir="joy-data"
+dst_data_set_dir="data-sets"
 joy="../joy/sleuth"
 
-echo $joy_dir
-echo $data_set_dir
+echo $src_joy_dir
+echo $dst_data_set_dir
 
-for f in $joy_dir/*
+for dir in $src_joy_dir/*
 do
- echo "Processing $f .." 
- filename=$(basename -- "$f")
- extension="${filename##*.}"
- name="${filename%.*}"
-$joy $f --where "da=192.168.3.213 | sa=192.168.3.213" --select "ip,pr,sa,da,sp,dp,byte_dist,packets,total_entropy,byte_dist_mean,entropy" > "$data_set_dir/$name.json"
+ dir_name=$(basename -- "$dir")
+ for f in $dir/*
+ do
+  echo "Processing $f .." 
+  filename=$(basename -- "$f")
+  extension="${filename##*.}"
+  name="${filename%.*}"
+ $joy $f --where "da=192.168.3.213 | sa=192.168.3.213" > "$dst_data_set_dir/$dir_name/$name.json"
+ done
 done
 
 
